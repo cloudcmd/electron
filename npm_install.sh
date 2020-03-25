a87890a6713a5808c05293b97bbed829c7e5a4cd
coderaiser <coderaiser@cloudcmd.io>
2020-03-25 21:28:54 +0200

    feature(package) electron v8.2.0

--- npm_install.sh
+++ npm_install.sh
@@ -1,5 +1,5 @@
 # Electron's version.
-export npm_config_target=7.1.7
+export npm_config_target=8.2.0
 # The architecture of Electron, can be ia32 or x64.
 export npm_config_arch=x64
 export npm_config_target_arch=x64
